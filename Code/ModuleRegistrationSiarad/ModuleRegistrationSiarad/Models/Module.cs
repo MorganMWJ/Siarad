1a12a5e8ca95e8ed1c074b7eee200cb57c9b3d84
dop2 <dop2@aber.ac.uk>
2019-11-28 11:22:08 +0000

    Added ModuleRegistrationCode - Still needs commenting and neatening

---
+++ Code/ModuleRegistrationSiarad/ModuleRegistrationSiarad/Models/Module.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModuleRegistrationSiarad.Models
+{
+    public class Module
+    {
+        public virtual string module_id { get; set; }
+        public virtual string year { get; set; }
+        [Required]
+        public virtual string coordinator_id { get; set; }//Coordinator
+        [Required]
+        public virtual string module_title { get; set; }
+    }
+   
+}
