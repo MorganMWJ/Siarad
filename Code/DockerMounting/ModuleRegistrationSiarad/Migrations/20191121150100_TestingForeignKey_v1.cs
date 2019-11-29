fc7cadfa803f6ed985dc14097f738b704c59f619
dop2 <dop2@aber.ac.uk>
2019-11-29 14:44:36 +0000

    Added ModuleRegistration for Docker mounting

---
+++ Code/DockerMounting/ModuleRegistrationSiarad/Migrations/20191121150100_TestingForeignKey_v1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ModuleRegistrationSiarad.Migrations
+{
+    public partial class TestingForeignKey_v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "year",
+                table: "staff",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_staff_module_id_year",
+                table: "staff",
+                columns: new[] { "module_id", "year" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_registered_students_user_id",
+                table: "registered_students",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_registered_students_module_id_year",
+                table: "registered_students",
+                columns: new[] { "module_id", "year" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_registered_students_students_user_id",
+                table: "registered_students",
+                column: "user_id",
+                principalTable: "students",
+                principalColumn: "user_id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_registered_students_module_module_id_year",
+                table: "registered_students",
+                columns: new[] { "module_id", "year" },
+                principalTable: "module",
+                principalColumns: new[] { "year", "module_id" },
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_staff_module_module_id_year",
+                table: "staff",
+                columns: new[] { "module_id", "year" },
+                principalTable: "module",
+                principalColumns: new[] { "year", "module_id" },
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_registered_students_students_user_id",
+                table: "registered_students");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_registered_students_module_module_id_year",
+                table: "registered_students");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_staff_module_module_id_year",
+                table: "staff");
+
+            migrationBuilder.DropIndex(
+                name: "IX_staff_module_id_year",
+                table: "staff");
+
+            migrationBuilder.DropIndex(
+                name: "IX_registered_students_user_id",
+                table: "registered_students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_registered_students_module_id_year",
+                table: "registered_students");
+
+            migrationBuilder.DropColumn(
+                name: "year",
+                table: "staff");
+        }
+    }
+}
---
+++ Code/DockerMounting/ModuleRegistrationSiarad/Migrations/20191129111732_ModuleRegistrationDatabase_v1_2.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ModuleRegistrationSiarad.Migrations
+{
+    public partial class ModuleRegistrationDatabase_v1_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_module",
+                table: "module");
+
+            migrationBuilder.RenameTable(
+                name: "module",
+                newName: "modules");
+
+            migrationBuilder.AddColumn<string>(
+                name: "class_code",
+                table: "modules",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_modules",
+                table: "modules",
+                columns: new[] { "module_id", "year", "class_code" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_modules",
+                table: "modules");
+
+            migrationBuilder.DropColumn(
+                name: "class_code",
+                table: "modules");
+
+            migrationBuilder.RenameTable(
+                name: "modules",
+                newName: "module");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_module",
+                table: "module",
+                columns: new[] { "module_id", "year" });
+        }
+    }
+}
